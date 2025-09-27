3cd7b12e8fa7f7ceaee4ad8a3d8d73981074399b
Stefany-viveiros <viveirosstefany@gmail.com>
2025-09-27 09:21:52 -0300

    Objeto- Automatização

---
+++ ExemploObjeto.py
@@ -0,0 +1,11 @@
+from models.Pessoa import Pessoa
+
+pessoa = Pessoa("Stefany", "viveiros@hotmail.com", "1234")
+
+pessoa.cadastrar()
+
+pessoa.sair()
+
+# Programação orientada a objetos
+# Aproximar o mundo real do mundo virtual
+
